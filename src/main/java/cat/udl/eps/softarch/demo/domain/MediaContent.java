dcf04b68661787652ff09f3e6823bf11d15f1ccf
Alex Lillo Lindo <alillolindo@gmail.com>
2026-02-17 17:37:39 +0100

    fix: fix reserved name use of type in Edition.java

--- src/main/java/cat/udl/eps/softarch/demo/domain/MediaContent.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/MediaContent.java
@@ -1,6 +1,7 @@
 package cat.udl.eps.softarch.demo.domain;
 
 import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
@@ -18,6 +19,7 @@ public class MediaContent {
     private String url;
 
     @NotBlank
+	@Column(name = "media_type")
     private String type;
 
     @ManyToOne
