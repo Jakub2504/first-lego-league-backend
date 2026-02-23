76d2d9a3e6b924dd6e345770cd2415ddb7b6aaac
Gerard Moliner <gerymoliner@gmail.com>
2026-02-23 20:18:48 +0100

    Test

--- src/test/java/cat/udl/eps/softarch/demo/repository/TestTeamRepository.java
+++
@@ -1,9 +0,0 @@
-package cat.udl.eps.softarch.demo.repository;
-
-import cat.udl.eps.softarch.demo.domain.Team;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "teams", path = "teams")
-public interface TestTeamRepository extends JpaRepository<Team, Long> {
-}
