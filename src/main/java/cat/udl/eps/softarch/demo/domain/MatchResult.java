31f69116839d0336373470b92f745bf704e64f62
Gerard Moliner <gerymoliner@gmail.com>
2026-02-23 21:40:18 +0100

    FixTest - TAB Done

--- src/main/java/cat/udl/eps/softarch/demo/domain/MatchResult.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/MatchResult.java
@@ -1,9 +1,9 @@
 package cat.udl.eps.softarch.demo.domain;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Min;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -16,36 +16,31 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 public class MatchResult extends UriEntity<Long> {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
-/**
- * The score obtained by the team in this match.
- * Must be a non-negative integer.
- */
-    @Min(value = 0, message = "Score cannot be negative")
-    private Integer score;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-/**
- * The match where this result was obtained.
- * Serialized as a URI reference to avoid infinite recursion.
- */
-/*
-@NotNull
-    @ManyToOne
-	@JsonIdentityReference(alwaysAsId = true)
-    private Match match;
-*/
+	/**
+	 * The score obtained by the team in this match.
+	 * Must be a non-negative integer.
+	 */
+	@Min(value = 0, message = "Score cannot be negative")
+	private Integer score;
 
-/**
- * The team that achieved this result.
- * Serialized as a URI reference to avoid infinite recursion.
- */
-/*
-@NotNull
-    @ManyToOne
+	@Override
+	public Long getId() {
+		return this.id;
+	}
+
+	/*
+	@NotNull
+	@ManyToOne
 	@JsonIdentityReference(alwaysAsId = true)
-    private Team team;
-*/
+	private Match match;
+
+	@NotNull
+	@ManyToOne
+	@JsonIdentityReference(alwaysAsId = true)
+	private Team team;
+	*/
 }
